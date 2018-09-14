e8d4dcbc5257a0648c9e0539a2bcbc408311960c
Amine Er-Roundi <errounda@ensipc456.ensimag.fr>
2018-09-14 11:35:47 +0200

    Class Options to implement

---
+++ src/Option.cpp
@@ -0,0 +1,20 @@
+#include "Option.hpp"
+
+Option::Option(){
+	T_=0;
+	size_=0;
+	nbTimeSteps_=0;
+}
+
+Option::Option(double T, int nbTimeSteps, int size){
+	T_ = T;
+	nbTimeSteps_ = nbTimeSteps;
+	size_=size;
+}
+
+Option::Option(const Option & option){
+	T_ = option.T_;
+	nbTimeSteps_ = option.nbTimeSteps_;
+		size_= option.size_;
+}
+
---
+++ src/OptionBasket.cpp
@@ -0,0 +1,12 @@
+#include "OptionBasket.hpp"
+
+OptionBasket::~OptionBasket(){}
+
+//    virtual double payoff(const PnlMat *path){
+//    	PnlVect *vect ;
+//    	pnl_mat_get_col(vect, path, nbTimeSteps_+1);
+//    	return pnl_vect_scalar_prod(vect, weights)- strike;
+//
+//    }
+//};
+//
