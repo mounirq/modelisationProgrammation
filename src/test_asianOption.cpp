7d1fbcca0395185fff7be78ee2e812d2ecd2c7d8
Mounir Qat <mounir.qat@ensimag.grenoble-inp.fr>
2018-09-17 12:06:13 +0200

    Test asian option

---
+++ src/test_asianOption.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "OptionAsian.hpp"
+
+using namespace std;
+
+int main()
+{
+    PnlVect * weights = pnl_vect_create_from_list(3, 0.3, 0.2, 0.5);
+    OptionAsian optionAsian = OptionAsian(4,2,3,10,weights);
+    int nbUnderlyings = 3;
+    PnlMat * past = pnl_mat_create(3, nbUnderlyings);
+    pnl_mat_set(past, 0, 0, 10);
+    pnl_mat_set(past, 0, 1, 11);
+    pnl_mat_set(past, 0, 2, 12);
+    pnl_mat_set(past, 1, 0, 9);
+    pnl_mat_set(past, 1, 1, 10);
+    pnl_mat_set(past, 1, 2, 11);
+    pnl_mat_set(past, 2, 0, 11);
+    pnl_mat_set(past, 2, 1, 9);
+    pnl_mat_set(past, 2, 2, 10);
+    double payOffReturned = optionAsian.payoff(past);
+
+    cout<<"The Payoff of the Asian Option has to be equal to 0.5 and is : " << payOffReturned;
+    return 0;
+}
