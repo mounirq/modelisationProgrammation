5b5c56151279e9eede44fa889bbea56cdf7f9137
Mounir Qat <mounir.qat@ensimag.grenoble-inp.fr>
2018-09-17 11:39:15 +0200

    Fixed test

--- src/Option.cpp
+++ src/Option.cpp
@@ -8,6 +8,12 @@ Option::Option(double T, int nbTimeSteps, int size){
 	size_=size;
 }
 
+Option::Option(){
+    T_ = 0;
+    nbTimeSteps_ = 0;
+    size_=0;
+}
+
 Option::Option(const Option & option){
 	T_ = option.T_;
 	nbTimeSteps_ = option.nbTimeSteps_;
