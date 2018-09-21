79e11a03081d86597ec3bb457c2366502530a76e
Yasmine Tidane <yasmine.tidane@ensimag.grenoble-inp.fr>
2018-09-21 15:25:37 +0200

    test P&L basket

--- test/test_PL_basket.cpp
+++ test/test_PL_basket.cpp
@@ -1,5 +1,52 @@
-//
-// Created by Yasmine Tidane on 21/09/2018.
-//
+#include <iostream>
+#include <string>
+#include <pnl/pnl_vector.h>
+#include <pnl/pnl_random.h>
+#include "../src/BasketOption.hpp"
+#include "../src/BlackScholesModel.hpp"
+#include "../src/MonteCarlo.hpp"
 
-#include "test_PL_basket.h"
+using namespace std;
+
+int main(int argc, char **argv) {
+    int size = 5;
+    double strike = 100.0;
+    PnlVect *initialSpots = pnl_vect_create_from_scalar(5, 100);
+    double maturity = 1;
+    PnlVect *volatilities = pnl_vect_create_from_scalar(5, 0.2);
+    double intrestRate = 0.04879;
+    double rho = 0.0;
+    PnlVect *weights = pnl_vect_create_from_scalar(5, 0.2);
+    int nbTimeSteps = 1;
+    BasketOption *optionBasket = new BasketOption(maturity, nbTimeSteps, size, strike, weights);
+
+    BlackScholesModel *bs = new BlackScholesModel(size, intrestRate, rho, volatilities, initialSpots);
+
+    double prix1;
+    double ic1;
+
+    int nbSamples = 10000;
+
+    PnlMat *market_trajectory = pnl_mat_create_from_file((char *)"../../data/market-data/simul_basket.dat");
+
+    //cout << "hell : " << market_trajectory->m << endl;
+    PnlRandom * rng = new PnlRandom();
+    MonteCarlo *mc = new MonteCarlo(bs, optionBasket, rng, 0.01, nbSamples);
+
+
+    double p_and_l;
+    mc->profits_and_losses(market_trajectory, p_and_l);
+    cout << "Le P&L est : " << p_and_l << endl;
+
+
+    pnl_vect_free(&initialSpots);
+    pnl_vect_free(&volatilities);
+    pnl_vect_free(&weights);
+    delete &rng;
+    pnl_mat_free(&market_trajectory);
+//    delete optionBasket;
+//    delete bs;
+//    delete mc;
+
+    exit(0);
+}
